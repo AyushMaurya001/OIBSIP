985d4637a25e6e6b1b012d9a2a56e8ada6b45c90
Ayush <120236359+AyushMaurya001@users.noreply.github.com>
2023-10-25 20:06:38 +0530

    Add files via upload

---
+++ Temperature Converter Website/script.js
@@ -0,0 +1,39 @@
+const celsiusField = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "0";
+  celsiusField.innerHTML = "";
+});
+
+if(degree.value === ""){
+  convertBtn.setAttribute("disabled","");
+  setTimeout(() => {
+    convertBtn.removeAttribute('disabled');
+  }, 0);
+}
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+  convertBtn.innerHTML = "Converting...";
+  setTimeout(() => {
+    convertBtn.innerHTML ="Convert"
+  }, 500);
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+  
+  setTimeout( () => {
+    if (tempType.value === "fahrenheit") {
+      const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+      celsiusField.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;C`;
+    } else if (tempType.value === "kelvin") {
+      const KelvinToCelsius = inputValue - 273.15;
+      celsiusField.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;C`;
+    }
+  }, 500)
+}
